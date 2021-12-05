7f0610b75111f1f7332cc83d691dce3abb6beebf
Burkay Durdu <burkaydurdu@outlook.com>
2021-12-06 00:55:36 +0300

    edit executables files

--- executables/connector.sh
+++ executables/connector.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Create Connector
+# Create Source Connector
 curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d "@connectors/source-connector.json"
 
-# Update Connector
+# Create Sink Connector
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d "@connectors/sink-connector.json"
+
+# Update Source Connector
 curl -i -X PUT -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/source-connector/config -d "@connectors/source-connector-edit.json"
+
+# Update Source Connector
+curl -i -X PUT -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/sink-connector/config -d "@connectors/sink-connector-edit.json"
--- executables/open.sh
+++ executables/open.sh
@@ -3,10 +3,13 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   xdg-open http://localhost:8080
   xdg-open http://localhost:8081
+  xdg-open http://localhost:8091
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   open http://localhost:8080
   open http://localhost:8081
+  open http://localhost:8091
 elif [[ "$OSTYPE" == "cygwin" ]]; then
   start http://localhost:8080
   start http://localhost:8081
+  start http://localhost:8091
 fi
